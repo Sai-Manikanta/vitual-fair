389092b8178809d3580300e05bb40c1a3db5ddce
mani333007 <mani333007@gmail.com>
2021-11-17 18:57:59 +0530

    added image map

--- src/components/Landing3dContent.js
+++ src/components/Landing3dContent.js
@@ -2,52 +2,34 @@ import LobbyVideo from '../videos/lobby.mp4';
 
 function Landing3dContent() {
     return (
-        <div className="flex-grow relative py-6 px-14">
+        <div className="flex-grow relative">
+            <img 
+                src="https://d246zmsm5ycwub.cloudfront.net/assets/images/LobbyIMAGEFORTESTING.png" 
+                alt="LobbyIMAGEFORTESTING"
+                className="absolute w-full h-full"
+                usemap="#image-map"
+            />
+
+            <map name="image-map">
+                <area target="_self" alt="Conference" title="Conference" href="conference" coords="170,350,441,488" shape="rect" />
+                <area target="_self" alt="Exibition" title="Exibition" href="exbition" coords="1002,496,1290,349" shape="rect" />
+                <area target="_self" alt="Meeting" title="Meeting" href="meeting" coords="206,339,445,277" shape="rect" />
+                <area target="_self" alt="Poster" title="Poster" href="Poster" coords="1000,341,1217,278" shape="rect" />
+                <area target="_self" alt="Information Desk" title="Information Desk" href="Information desk" coords="653,482,785,400" shape="rect" />
+                <area target="_self" alt="Pitstop" title="Pitstop" href="Pitstop" coords="677,577,762,549" shape="rect" />
+            </map>
+
             <video 
-                autoPlay 
-                loop 
+                autoPlay
+                loop
                 muted
-                style={{
-                    position: 'absolute',
-                    width: '100%',
-                    left: '50%',
-                    top: '50%',
-                    height: '100%',
-                    objectFit: 'fill',
-                    transform: 'translate(-50%, -50%)',
-                    zIndex: '-1', 
-                }}
+                style={{ width: '100%', height: '100%', objectFit: 'fill', objectPosition: 'top', position: 'absolute', top: 0, left: 0, zIndex: '-1' }} 
             >
                 <source src={LobbyVideo} type="video/mp4" />
+                Your browser does not support HTML5 video.
             </video>
-            <div 
-                className="grid grid-cols-3 gap-7"
-            >
-                <div className="h-60 bg-green-500"></div>
-                <div className="h-60 bg-green-500"></div>
-                <div className="h-60 bg-green-500"></div>
-            </div>
         </div>
     )
 }
-
-// style={{
-//     position: 'absolute',
-//     width: '100%',
-//     left: '50%',
-//     top: '50%',
-//     height: '100%',
-//     objectFit: 'cover',
-//     transform: 'translate(-50%, -50%)',
-//     zIndex: '-1'
-// }}
-
-// style={{
-//     position: 'absolute',
-//     height: '100%',
-//     width: '100%',
-//     objectFit: 'cover',
-//     zIndex: '-1'
-// }}
 
 export default Landing3dContent
